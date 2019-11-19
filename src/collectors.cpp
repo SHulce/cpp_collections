ad439dde8646fc7421b53de1f93d3dd186c2ec83
Shawn Hulce <shulce@asu.edu>
2019-11-19 14:13:21 -0600

    Added in basic stack and additional testing

--- src/collectors.cpp
+++ src/collectors.cpp
@@ -2,16 +2,17 @@
 #include <string>
 
 namespace collectors {
+    //Vector Impl
     template <typename T>
     vector<T>::vector() {
         storage = new T[DEFAULT_SIZE];
-        current_size = DEFAULT_SIZE;
+        current_capacity = DEFAULT_SIZE;
         current_index = 0;
     }
 
     template <typename T>
     T vector<T>::get(int index) {
-        if (index >= current_size) {
+        if (index >= current_capacity) {
             throw 0;
         }
 
@@ -25,12 +26,12 @@ namespace collectors {
 
     template <typename T>
     int vector<T>::capacity() {
-        return current_size;
+        return current_capacity;
     }
 
     template <typename T>
     void vector<T>::push_back(T item) {
-        if (current_index > current_size / 2) {
+        if (current_index > current_capacity / 2) {
             grow_storage();
         }
 
@@ -40,17 +41,73 @@ namespace collectors {
 
     template <typename T>
     void vector<T>::grow_storage() {
-        T* temp = new T[current_size * 2];
+        T* temp = new T[current_capacity * 2];
         for (int i = 0; i < current_index; i++) {
             temp[i] = storage[i];
         }
-        current_size *= 2;
+        current_capacity *= 2;
         delete[] storage;
         storage = temp;
     }
+    //End Vector Impl
+    //Stack Impl
+    template <typename T>
+    stack<T>::stack() {
+        storage = new T[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+        size = 0;
+    }
+
+    template <typename T>
+    void stack<T>::push(T value) {
+        if (size > capacity / 2) {
+            grow_storage();
+        }
+        storage[size] = value;
+        size++;
+    }
+
+    template <typename T>
+    T stack<T>::pop() {
+        size--;
+        return storage[size];
+    }
+
+    template <typename T>
+    T stack<T>::peek() {
+        return storage[size - 1];
+    }
+
+    template <typename T>
+    void stack<T>::grow_storage() {
+        T* temp = new T[capacity * 2];
+        for (int i = 0; i < size; i++) {
+            temp[i] = storage[i];
+        }
+        capacity *= 2;
+        delete[] storage;
+        storage = temp;
+    }
+
+    template <typename T>
+    int stack<T>::count() {
+        return size;
+    }
+
+    template <typename T>
+    bool stack<T>::empty() {
+        return size == 0;
+    }
+
+    //End Stack Impl
 }
 
 template class collectors::vector<int>;
 template class collectors::vector<std::string>;
 template class collectors::vector<double>;
 template class collectors::vector<float>;
+
+template class collectors::stack<int>;
+template class collectors::stack<std::string>;
+template class collectors::stack<double>;
+template class collectors::stack<float>;
